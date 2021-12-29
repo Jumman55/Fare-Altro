cbb4b086db1cb03711cf4f872390155fd0a53182
Jumman5 <mhjummanan@gmail.com>
2021-12-29 17:04:37 +0100

    Initial Commit

---
+++ Fare Altro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fare Altro
+//
+//  Created by Jumman Hossen on 29/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fare Altro/Fare_AltroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fare_AltroApp.swift
+//  Fare Altro
+//
+//  Created by Jumman Hossen on 29/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Fare_AltroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
